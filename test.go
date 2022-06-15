027c9ec0a18fca06fb6f16f25accef218c33d516
Yevhen Oshkukov <yoshkukov@Yevhens-MacBook-Pro.local>
2022-06-15 21:53:14 +0300

    second commit

--- test.go
+++ test.go
@@ -7,7 +7,7 @@ func main() {
 	var onePear int = 7
 	var allMoney int = 23
 
-	fmt.Println("Щоб купити 9 яблук та 8 груш, треба витратити", oneApple*float64(onePear), "грн.") // 41.93 грн.
+	fmt.Println("Щоб купити 9 яблук та 8 груш, треба витратити", oneApple*float64(onePear), "грн.") //  41.93 грн.
 	fmt.Println("На наші гроші ми можемо купити", allMoney/onePear, "груші")                        //  3 груші
 	fmt.Println("На наші гроші ми можемо купити", int(float64(allMoney)/oneApple), "яблука")        //  3 яблука
 	fmt.Println("Чи ми можемо купити 2 яблука та 2 груші", allMoney >= int(2*oneApple)+2*onePear)   //  false
